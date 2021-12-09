f1775c1b703edfecfa5ca0168ffd8d86a4d94dd5
amyalkatib <91368588+amyalkatib@users.noreply.github.com>
2021-12-08 20:08:27 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,32 +6,32 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 #Fill in start
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
-#print 'the web server is up on port:',serverPort
+#print('the web server is up on port:',serverPort)
 #Fill in end
 while True:
  #Establish the connection
- #print 'Ready to serve...'
+ #print('Ready to serve...')
  connectionSocket, addr = serverSocket.accept()
 
- try:
+ #try:
 
- message = connectionSocket.recv(1024)
- #print message,'::',message.split()[0],':',message.split()[1]
- filename = message.split()[1]
- #print filename,'||',filename[1:]
- f = open(filename[1:])
- outputdata = f.read()
- #print outputdata
+    #message = connectionSocket.recv(1024)
+ #print (message,':',message.split()[0],':',message.split()[1])
+ #filename = helloworld.html
+ #print(message)
+ #f = open(filename[1:])
+ #outputdata = f.read()
+ #print (outputdata)
  #Send one HTTP header line into socket
  #Fill in start
- connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
- connectionSocket.send(outputdata)
+ #connectionSocket.send(HTTP/1.1 200 OK)
+ #connectionSocket.send(outputdata)
  #Fill in end
  #Send the content of the requested file to the client
- for i in range(0, len(outputdata)):
- connectionSocket.send(outputdata[i])
+ #for i in range(0, len(outputdata)):
+ #connectionSocket.send(outputdata[i])
  connectionSocket.close()
- except IOError:
+ #except IOError:
  #Send response message for file not found
  #Fill in start
  connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
