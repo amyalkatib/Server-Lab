7c59ad0ad2b96e407a42f67614d65389d0194450
amyalkatib <91368588+amyalkatib@users.noreply.github.com>
2021-12-15 01:09:12 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,39 +13,38 @@ def webServer(port=13331):
     while True:
         # Establish the connection
         # print('Ready to serve...')
-        connectionSocket, addr =  serverSocket.accept()      #Fill in end
+        connectionSocket, addr =  serverSocket.accept()      #Fill in e
         try:
-            try:
-                message =  connectionSocket.recv(1024).decode()# Fill in start    #Fill in end
-                filename = message.split()[1]
+            message =  connectionSocket.recv(1024).decode()# Fill in start    #Fill in end
+            filename = message.split()[1]
                 #with open(path, encoding="utf8", errors='ignore') as f:
-                f = open(filename)
+            f = open(filename)
                 
-                outputdata = f.read()# Fill in start     #Fill in end
+            outputdata = f.read()# Fill in start     #Fill in end
                 # Send one HTTP header line into socket.
-                connectionSocket.send("HTTP/1.1 404 not found\r\n\r\n".encode())
+            connectionSocket.send("HTTP/1.1 404 not found\r\n\r\n".encode())
                 # Fill in start
                 # Fill in end
                 # Send the content of the requested file to the client
-                for i in range(0, len(outputdata)):
-                    connectionSocket.send(outputdata[i].encode())
-                    i = i + 1
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
+                i = i + 1
                 connectionSocket.send("\r\n".encode())
                 connectionSocket.close()
-            except IOError:
+        except IOError:
         # Send response message for file not found (404)
         # Fill in start
-                connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+            connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
 
-                connectionSocket.send("<html><head></head><body><h1>200 OK</h1></body></html>\r\n".encode())
-                connectionSocket.send("HTTP/1.1 404not found\r\n\r\n".encode())
+            connectionSocket.send("<html><head></head><body><h1>200 OK</h1></body></html>\r\n".encode())
+            connectionSocket.send("HTTP/1.1 404not found\r\n\r\n".encode())
 
-                connectionSocket.send("<html><head></head><body><h1>404 not found</h1></body></html>\r\n".encode())
+            connectionSocket.send("<html><head></head><body><h1>404 not found</h1></body></html>\r\n".encode())
                 
         # Fill in end
 
         # Close client socket
-                connectionSocket.close()
+             connectionSocket.close()
         # Fill in start
         # Fill in end
         except (ConnectionResetError, BrokenPipeError):
